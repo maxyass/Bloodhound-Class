e5ae69877d9dd144e165e9451fac628abe3a319a
max <maxjyass@gmail.com>
2025-10-18 14:45:51 -0500

    updated

--- install.sh
+++ install.sh
@@ -15,27 +15,25 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 apt update
 apt-get install -y --no-install-recommends docker-ce docker-compose-plugin
 
-
-### Docker Install
-
+# Remove any conflicting packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
+# Add Docker's official GPG key
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# Add the repository to Apt sources
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Install Docker
 sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-git clone https://github.com/SpecterOps/BloodHound.git
-cd BloodHound/examples/docker-compose
-cp docker-compose.yml docker-compose.bak
-cp .env.example .env
-sed -i 's/BLOODHOUND_HOST=127.0.0.1/BLOODHOUND_HOST=0.0.0.0/' .env
-sudo docker compose up 
+# Test the installation
+sudo docker run hello-world\
