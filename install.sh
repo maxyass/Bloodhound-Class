adbc43219e797f793cda9a48f061dbcd7dfcd3cf
max <maxjyass@gmail.com>
2025-10-18 14:47:26 -0500

    drop hello

--- install.sh
+++ install.sh
@@ -35,5 +35,3 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Test the installation
-sudo docker run hello-world\
